d775a8160e4a2cac4e8ac1f2dcef09e555f07483
Kim-SuBin <sb2504@naver.com>
2022-06-05 20:47:58 +0900

    [Feat] ✨ Add Send Chat Feature (#2)
    채팅을 보내기 위한 기능 구현

--- src/main/java/com/demo/chatting/constants/KafkaConstants.java
+++ src/main/java/com/demo/chatting/constants/KafkaConstants.java
@@ -5,6 +5,6 @@ package com.demo.chatting.constants;
  */
 public class KafkaConstants {
     public static final String KAFKA_TOPIC = "kafka-demo-chat";
-    public static final String GROUP_ID = "foo";
+    public static final String GROUP_ID = "demo-chat";
     public static final String KAFKA_BROKER = "localhost:9092";
 }
