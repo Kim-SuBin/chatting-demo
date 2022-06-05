791a7e02ddae5957c939060c50fb154a8fcf7a19
Kim-SuBin <sb2504@naver.com>
2022-06-05 15:55:11 +0900

    [Feat] ✨ Create Kafka Topic
    kafka-demo-chat 이라는 이름으로 Kafka Topic 생성

---
+++ src/main/java/com/demo/chatting/constants/KafkaConstants.java
@@ -0,0 +1,10 @@
+package com.demo.chatting.constants;
+
+/**
+ *
+ */
+public class KafkaConstants {
+    public static final String KAFKA_TOPIC = "kafka-demo-chat";
+    public static final String GROUP_ID = "foo";
+    public static final String KAFKA_BROKER = "localhost:9092";
+}
