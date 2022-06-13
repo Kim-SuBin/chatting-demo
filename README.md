5a180470d832aa98055c7daf0ff3bb1e43060fc4
Kim-SuBin <sb2504@naver.com>
2022-06-13 18:02:51 +0900

    [Update] ⚙️ project setting init
    프로젝트를 처음부터 다시 만들기 위해 모든 코드 및 설정 초기화

--- README.md
+++
@@ -1,2 +0,0 @@
-# chatting-demo
-1 대 1 채팅 시스템 데모
--- src/test/java/com/demo/chatting/api/ApiApplicationTests.java
+++ src/test/java/com/demo/chatting/api/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.demo.chatting;
+package com.demo.chatting.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChattingDemoApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
